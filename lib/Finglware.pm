cd5b3f4bf0d40649662eb3d915a6a697b0070f85
Brad Haywood <brad@geeksware.dlink.com>
2012-12-07 16:22:07 +0000

    Moved all modifier methods out of core and into hooks plugin

--- lib/Finglware.pm
+++ lib/Finglware.pm
@@ -251,44 +251,6 @@ sub import {
     };
 }
 
-sub after {
-    my ($name, $code) = @_;
-
-    Class::LOP->init(caller())->add_hook(
-        type    => 'after',
-        name    => $name,
-        method  => $code,
-    );
-}
-
-sub before {
-    my ($name, $code) = @_;                                                                                                                                                                                           
-
-    Class::LOP->init(caller())->add_hook(
-        type    => 'before',
-        name    => $name,
-        method  => $code,
-    );
-}
-
-sub around {
-    my ($name, $code) = @_;                                                                                                                                                                                           
-    Class::LOP->init(caller())->add_hook(
-        type    => 'around',
-        name    => $name,
-        method  => $code,
-    );
-}
-
-sub override {
-    my ($name, $code) = @_;
-    
-    Class::LOP->init(caller())->override_method(
-        $name,
-        $code
-    );
-}
-
 sub attr {
     my $class = scalar caller();
     for my $acc (@_) {
--- lib/Finglware/Plugin/Hooks.pm
+++ lib/Finglware/Plugin/Hooks.pm
@@ -5,12 +5,49 @@ sub import {
     my $caller = caller;
     # need to find a better way of doing this
     # to make it easier when creating plugins!
-    Class::LOP->init('Finglware')->import_methods($caller, qw(
+    Class::LOP->init($class)->import_methods($caller, qw(
         after
         before
         around
         override
     )) unless scalar($caller) eq 'Finglware';
+}
+
+sub after {
+    my ($name, $code) = @_;
+
+    Class::LOP->init(caller())->add_hook(
+        type    => 'after',
+        name    => $name,
+        method  => $code,
+    );
+}
+
+sub before {
+    my ($name, $code) = @_;                                                                                                                       
+    Class::LOP->init(caller())->add_hook(
+        type    => 'before',
+        name    => $name,
+        method  => $code,
+    );
+}
+
+sub around {
+    my ($name, $code) = @_;                                                                                                                       
+    Class::LOP->init(caller())->add_hook(
+        type    => 'around',
+        name    => $name,
+        method  => $code,
+    );
+}
+
+sub override {
+    my ($name, $code) = @_;
+
+    Class::LOP->init(caller())->override_method(
+        $name,
+        $code
+    );
 }
 
 1;
